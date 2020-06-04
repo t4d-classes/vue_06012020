394039e92809ba5282c794affe952d0fbf664585
Eric Greene <eric@t4d.io>
2020-06-04 13:12:45 -0400

    add code demos

--- demo-app/public/app.js
+++ demo-app/public/app.js
@@ -27,46 +27,96 @@
 //   }
 // }
 
+// const step1 = () => {
 
-// console.log('creating promise');
+//   return new Promise((resolve, reject) => {
 
-// const p = new Promise((resolve, reject) => {
-
-//   console.log('calling host function: setTimeout');
-
-//   setTimeout(() => {
-//     console.log('calling reject');
-//     reject('some error');
-//   }, 2000);
-
-// });
-
-
-// p
-//   .then((result) => {
-//     console.log('resolved:', result);
-//   })
-//   .catch(result => {
-//     console.log('rejected:', result);
+//     setTimeout(() => {
+//       resolve('step 1 complete');
+//     }, 2000);
+  
 //   });
 
-// console.log('waiting');
+// };
 
-const xhr = new XMLHttpRequest();
+// const step2 = () => {
 
-xhr.addEventListener('readystatechange', () => {
+//   return new Promise((resolve, reject) => {
 
-  if (xhr.status === 200 && xhr.readyState === 4) {
-    console.log(JSON.parse(xhr.responseText));
-  }
+//     setTimeout(() => {
+//       reject('step 2 error');
+//     }, 2000);
+  
+//   });
 
-});
+// };
 
-xhr.open('GET', 'http://localhost:3070/cars');
-xhr.send();
+// const step3 = () => {
+
+//   return new Promise((resolve, reject) => {
+
+//     setTimeout(() => {
+//       resolve('step 3 complete');
+//     }, 2000);
+  
+//   });
+
+// };
+
+// step1()
+//   .then(result => {
+//     console.log(result);
+//     return step2();
+//   })
+//   .then(result => {
+//     console.log(result);
+//     return step3();
+//   })
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+
+
+// const myFetch = (url) => {
+
+//   return new Promise(resolve => {
+
+//     const xhr = new XMLHttpRequest();
+
+//     xhr.addEventListener('readystatechange', () => {
+    
+//       if (xhr.status === 200 && xhr.readyState === 4) {
+//         resolve(JSON.parse(xhr.responseText));
+//       }
+    
+//     });
+    
+//     xhr.open('GET', url);
+//     xhr.send();
+
+//   });
+
+// };
+
 
 // Exercise
 // Implement the following myFetch function, I should be able to take this code and paste into your file, and the code works.
 // The cars variable should be populated with an array of cars.
 
-myFetch('http://localhost:3070/cars').then(cars => console.log(cars));
+// myFetch('http://localhost:3070/cars').then(cars => console.log(cars));
+
+// fetch('http://localhost:3070/cars')
+//   .then(res => res.json())
+//   .then(cars => console.log(cars));
+
+fetch('http://localhost:3070/cars', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ make: 'Ford', model: 'T', year: 1920, color: 'black', price: 400 }),
+})
+  .then(res => res.json())
+  .then(car => console.log(car));
