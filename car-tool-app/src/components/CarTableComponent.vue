6abc81fc502cb3094976e95b9e0a379f1eda0bb5
Eric Greene <eric@t4d.io>
2020-06-04 09:21:12 -0400

    add code demos

--- car-tool-app/src/components/CarTableComponent.vue
+++ car-tool-app/src/components/CarTableComponent.vue
@@ -5,13 +5,13 @@
         <th>Id</th>
         <th>
           <button @click="sortCars('make')">
-            <label v-if="editCarId > 0" v-once :for="fieldId('make')">Make</label>
+            <label v-if="isEditing" v-once :for="fieldId('make')">Make</label>
             <span v-else>Make</span>
           </button>
         </th>
         <th>
           <button @click="sortCars('model')">
-            <label v-if="editCarId > 0" v-once :for="fieldId('model')">Model</label>
+            <label v-if="isEditing" v-once :for="fieldId('model')">Model</label>
             <span v-else>Model</span>
           </button>
         </th>
@@ -23,13 +23,13 @@
         </th>
         <th>
           <button @click="sortCars('color')">
-            <label v-if="editCarId > 0" v-once :for="fieldId('color')">Color</label>
+            <label v-if="isEditing" v-once :for="fieldId('color')">Color</label>
             <span v-else>Color</span>
           </button>
         </th>
         <th>
           <button @click="sortCars('price')">
-            <label v-if="editCarId > 0" v-once :for="fieldId('price')">Price</label>
+            <label v-if="isEditing" v-once :for="fieldId('price')">Price</label>
             <span v-else>Price</span>
           </button>
         </th>
@@ -73,6 +73,9 @@ export default {
     tableId() {
       return uniqueId();
     },
+    isEditing() {
+      return this.editCarId > 0;
+    }
   },
   methods: {
     sortCars(fieldName) {
